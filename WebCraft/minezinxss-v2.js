f6e3b5f1892ff54567273a70496b28fca3305379
Breno Santos <brenosantos145@gmail.com>
2025-05-15 15:03:19 -0300

    chore: v2

---
+++ WebCraft/minezinxss-v2.js
@@ -0,0 +1,31 @@
+function inject() {
+  console.log("Stager: Injecting iframe...");
+  document.title = "MINEZIN da GARELA > DOM";
+
+  document.getElementsByTagName("body")[0].innerHTML =
+    '<iframe src="https://eaglercraft.com/mc/1.8.8" style="position: absolute; width: 100%; height: 100%; border: none;"></iframe>';
+}
+
+if (
+  document.readyState === "complete" ||
+  document.readyState === "interactive"
+) {
+  console.log("Stager: DOM ready");
+  inject();
+} else if (window.attachEvent) {
+  console.log("Stager: Attaching to onload");
+  window.attachEvent("onload", inject);
+} else if (window.addEventListener) {
+  console.log("Stager: Attaching to load");
+  window.addEventListener("load", inject, false);
+} else {
+  console.log("Stager: Fallback to iframe count");
+  let fc = document.getElementsByTagName("iframe").length;
+  setInterval(function () {
+    if (document.getElementsByTagName("iframe").length != fc) {
+      return;
+    } else {
+      inject();
+    }
+  }, 300);
+}
